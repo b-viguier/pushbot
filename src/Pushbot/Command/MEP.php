7bc0323f024ae181ba196bc9bcd1dab1705f2e33
Benoit Viguier <bviguier.externe@m6.fr>
2016-11-06 16:12:53 +0100

    Added 'mep' command

---
+++ src/Pushbot/Command/MEP.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace M6\Pushbot\Command;
+
+use M6\Pushbot\Deployment;
+use M6\Pushbot\Response;
+
+class MEP implements CommandInterface
+{
+    public function execute(Deployment\Pool $pool, string $user, array $args) : Response
+    {
+        $projectName = reset($args);
+        if(!$projectName) {
+            $response = $this->help();
+            $response->status = Response::FAILURE;
+
+            return $response;
+        }
+
+        $queue = $pool[$projectName] ?? $pool[$projectName] = new Deployment\Queue();
+        $queue[] = new Deployment($user);
+
+        return new Response(
+            Response::SUCCESS,
+            'Done'
+        );
+    }
+
+    public function help() : Response
+    {
+        return new Response(
+            Response::SUCCESS,
+            'mep <project>'
+        );
+    }
+
+}
