7442d367147643002c4cbe951b4c35441f654276
Benoit Viguier <bviguier.externe@m6.fr>
2016-11-29 13:41:05 +0100

    Added PhpArray persister

--- src/Pushbot/Deployment/Pool/Persister/JsonFile.php
+++ src/Pushbot/Deployment/Pool/Persister/JsonFile.php
@@ -5,7 +5,6 @@ namespace M6\Pushbot\Deployment\Pool\Persister;
 use M6\Pushbot\Deployment;
 use M6\Pushbot\Deployment\Pool;
 use M6\Pushbot\Deployment\Pool\PersisterInterface;
-use M6\Pushbot\Deployment\Queue;
 
 class JsonFile implements PersisterInterface
 {
@@ -18,32 +17,21 @@ class JsonFile implements PersisterInterface
 
     public function load(Pool $pool)
     {
-        $data = json_decode(
+        $arrayPersister = new PhpArray();
+        $arrayPersister->data = json_decode(
             @file_get_contents($this->filename),
             true
         ) ?? [];
-        foreach($data as $project => $queue) {
-            $pool[$project] = new Queue;
-            foreach($queue as $deployment) {
-                $pool[$project][] = new Deployment($deployment['user']);
-            }
-        }
+        $arrayPersister->load($pool);
     }
 
     public function save(Pool $pool)
     {
-        $data = [];
-        foreach($pool as $project => $queue) {
-            $data[$project] = [];
-            foreach($queue as $deployment) {
-                $data[$project][] = [
-                    'user' => $deployment->user,
-                ];
-            }
-        }
+        $arrayPersister = new PhpArray();
+        $arrayPersister->save($pool);
         file_put_contents(
             $this->filename,
-            json_encode($data)
+            json_encode($arrayPersister->data)
         );
     }
 }
---
+++ src/Pushbot/Deployment/Pool/Persister/PhpArray.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace M6\Pushbot\Deployment\Pool\Persister;
+
+use M6\Pushbot\Deployment;
+use M6\Pushbot\Deployment\Pool;
+use M6\Pushbot\Deployment\Pool\PersisterInterface;
+use M6\Pushbot\Deployment\Queue;
+
+class PhpArray implements PersisterInterface
+{
+    public $data;
+
+    public function load(Pool $pool)
+    {
+        foreach($this->data as $project => $queue) {
+            $pool[$project] = new Queue;
+            foreach($queue as $deployment) {
+                $pool[$project][] = new Deployment($deployment['user']);
+            }
+        }
+    }
+
+    public function save(Pool $pool)
+    {
+        $this->data = [];
+        foreach($pool as $project => $queue) {
+            $this->data[$project] = [];
+            foreach($queue as $deployment) {
+                $this->data[$project][] = [
+                    'user' => $deployment->user,
+                ];
+            }
+        }
+    }
+}
